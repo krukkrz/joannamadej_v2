0296cd771aefc39b08b8996690b6e548a4ae3008
Krzysztof Kruk <krzysztof.kruk@samsara.com>
2023-10-27 15:46:45 +0200

    PoC: routing and styling

---
+++ src/pages/Mvp.tsx
@@ -0,0 +1,14 @@
+import MVPComponent from "../components/mvp/MVPComponent";
+import MVP2Component from "../components/mvp2/MPV2";
+
+const Mvp = () => {
+    return (
+        <div>
+        <h1>MVP</h1>
+            <MVPComponent/>
+            <MVP2Component/>
+        </div>
+    );
+}
+
+export default Mvp;
