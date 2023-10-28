64e315e54d9f5231017c0eaff2b8f4d3604a063a
Krzysztof Kruk <krzysztof.kruk@samsara.com>
2023-10-28 16:11:16 +0200

    contact: implementation XL

---
+++ src/components/contact/Contact.tsx
@@ -0,0 +1,30 @@
+import phone from "../../assets/icons/phone.png";
+import paperPlane from "../../assets/icons/paper_plane.png";
+import linkedin from "../../assets/icons/linkedin.png";
+
+import styles from "./Contact.module.css";
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <div className={styles.contact}>
+        <hr />
+        <div className={styles.item}>
+          <img src={phone} alt="phone" /> +48 795 597 310
+        </div>
+        <hr />
+        <div className={styles.item}>
+          <img src={paperPlane} alt="paper_plane" /> joannamonikamadej@gmail.com
+        </div>
+        <hr />
+        <div className={styles.item}>
+          <img src={linkedin} alt="paper_plane" /> /jomadej
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import MyProjects from "../components/my_project/MyProjects";
 import AboutMe from "../components/about_me/AboutMe";
 import styles from "./Home.module.css";
 import Testimonials from "../components/testimonials/Testimonials";
+import Contact from "../components/contact/Contact";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
         <MyProjects />
         <AboutMe />
         <Testimonials />
+        <Contact />
       </div>
       <br />
       <br />
