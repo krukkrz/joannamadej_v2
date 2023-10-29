327f4c6b7f1ac8d6a37a1d83b415ef53b73ed786
Krzysztof Kruk <krzysztof.kruk@samsara.com>
2023-10-29 12:48:54 +0100

    my projects: xs

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,18 +3,18 @@ import styles from "./Navbar.module.scss";
 import hamburger from "../../assets/icons/hamburger.svg";
 const Navbar = () => {
   return (
-      <>
-        <div className={styles.navbar}>
-          <Link to={"/"}>Home</Link>
-          <Link to={"/"}>My projects</Link>
-          <Link to={"/"}>About me</Link>
-          <Link to={"/"}>Testimonials</Link>
-          <Link to={"/"}>Contact</Link>
-        </div>
-          <div className={styles.hamburger}>
-              <img src={hamburger} alt="hamburger" />
-          </div>
-      </>
+    <>
+      <div className={styles.navbar}>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/"}>My projects</Link>
+        <Link to={"/"}>About me</Link>
+        <Link to={"/"}>Testimonials</Link>
+        <Link to={"/"}>Contact</Link>
+      </div>
+      <div className={styles.hamburger}>
+        <img src={hamburger} alt="hamburger" />
+      </div>
+    </>
   );
 };
 
