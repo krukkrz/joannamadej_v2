22252659c9b406fa4dd9038d15ace43617965fc7
Krzysztof Kruk <krzysztof.kruk@samsara.com>
2023-10-29 12:37:05 +0100

    navbar: XS

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.scss";
-
+import hamburger from "../../assets/icons/hamburger.svg";
 const Navbar = () => {
   return (
-    <div className={styles.navbar}>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/"}>My projects</Link>
-      <Link to={"/"}>About me</Link>
-      <Link to={"/"}>Testimonials</Link>
-      <Link to={"/"}>Contact</Link>
-    </div>
+      <>
+        <div className={styles.navbar}>
+          <Link to={"/"}>Home</Link>
+          <Link to={"/"}>My projects</Link>
+          <Link to={"/"}>About me</Link>
+          <Link to={"/"}>Testimonials</Link>
+          <Link to={"/"}>Contact</Link>
+        </div>
+          <div className={styles.hamburger}>
+              <img src={hamburger} alt="hamburger" />
+          </div>
+      </>
   );
 };
 
